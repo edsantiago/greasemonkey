dc5fe260bea7d1f5661ff442ebf61f08409114bf
Eduardo Santiago <ed@edsantiago.com>
2020-10-26 15:58:38 -0600

    github-ci-highlight: remove spaces between tokens
    This actually makes everything look a lot better, because the
    color blocks abut one another.
    
    Signed-off-by: Eduardo Santiago <ed@edsantiago.com>

--- github-ci-highlight/github-ci-highlight.user.js
+++ github-ci-highlight/github-ci-highlight.user.js
@@ -5,13 +5,14 @@
 // @description highlight 'sys/int podman/remote fedora/ubuntu root/rootless'
 // @include     /.*/containers/podman/pull/
 // @require     https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
-// @version     0.01
+// @version     0.02
 // @grant       none
 // ==/UserScript==
 
 /*
 ** Changelog:
 **
+**  2020-10-26  0.02  remove space between tokens, make color blocks abut.
 **  2020-10-26  0.01  initial revision
 */
 
@@ -26,13 +27,13 @@ function add_css() {
     style.innerHTML = `
 .ci-int       { padding: 2px; background: #960; }
 .ci-sys       { padding: 2px; background: #cf9; }
-.ci-podman    { }
+.ci-podman    { padding: 2px; }
 .ci-remote    { padding: 2px; background: #f9f; }
 .ci-fedora    { padding: 2px; background: #294172; color: #adf; }
 .ci-ubuntu    { padding: 2px; background: #e95420; color: #fff; }
-.ci-root      { }
+.ci-root      { padding: 2px; }
 .ci-rootless  { padding: 2px; background: #ccc; color: #333; }
-.ci-host      { }
+.ci-host      { padding: 2px; }
 .ci-container { padding: 2px; background: #9cf; }
 `;
 
@@ -55,10 +56,10 @@ function add_css() {
 function highlight_int_sys_etc(element) {
     element.innerHTML = element.innerHTML.replace(/(int|sys)\s+(podman|remote)\s+((fedora|ubuntu)-\S+)\s+(root|rootless)\s+(host|container)/, function(match, intsys, client, full_os, os, root, hostcontainer) {
         var newhtml = "<span class=\"ci-"+intsys+"\">"+intsys+"</span>";
-        newhtml += " <span class=\"ci-"+client+"\">"+client+"</span>";
-        newhtml += " <span class=\"ci-"+os+"\">"+full_os+"</span>";
-        newhtml += " <span class=\"ci-"+root+"\">"+root+"</span>";
-        newhtml += " <span class=\"ci-"+hostcontainer+"\">"+hostcontainer+"</span>";
+        newhtml += "<span class=\"ci-"+client+"\">"+client+"</span>";
+        newhtml += "<span class=\"ci-"+os+"\">"+full_os+"</span>";
+        newhtml += "<span class=\"ci-"+root+"\">"+root+"</span>";
+        newhtml += "<span class=\"ci-"+hostcontainer+"\">"+hostcontainer+"</span>";
         return newhtml;
     });
 }
