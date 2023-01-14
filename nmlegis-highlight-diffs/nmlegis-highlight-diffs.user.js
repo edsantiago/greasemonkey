df78500393995bf02b2194057125157d301533b6
Eduardo Santiago <ed@edsantiago.com>
2023-01-14 07:42:36 -0700

    nmlegis-highlight-diffs : new script for nmlegis session
    Signed-off-by: Eduardo Santiago <ed@edsantiago.com>

---
+++ nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         nmlegis-highlight-diffs
+// @namespace    https://github.com/edsantiago/greasemonkey
+// @downloadURL  https://raw.githubusercontent.com/edsantiago/greasemonkey/master/nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
+// @description  highlight diffs in nmlegis.gov bills
+// @include      https://nmlegis.gov/*/bills/*.html
+// @version      1.0
+// @grant        none
+// ==/UserScript==
+
+/*
+** Changelog
+**
+**  2023-01-14  1.0  Initial version
+*/
+var allSpans, thisSpan;
+
+// line-through = deleted text
+allSpans = document.evaluate(
+    '//span[contains(@style,"line-through")]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null);
+for (var i = 0; i < allSpans.snapshotLength; i++) {
+    thisSpan = allSpans.snapshotItem(i);
+    thisSpan.style["background"] = "#ccc";
+}
+
+// underline = added text
+allSpans = document.evaluate(
+    '//span[contains(@style,"underline")]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null);
+for (var i = 0; i < allSpans.snapshotLength; i++) {
+    thisSpan = allSpans.snapshotItem(i);
+    thisSpan.style["background"] = "#930";
+    thisSpan.style["font-weight"] = "bold";
+}
