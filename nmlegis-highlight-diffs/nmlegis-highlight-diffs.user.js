1dc1388feae373bfb111973417197dd3f6ba07a0
Eduardo Santiago <ed@edsantiago.com>
2023-01-14 13:55:20 -0700

    linkify bill name in title
    Signed-off-by: Eduardo Santiago <ed@edsantiago.com>

--- nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
+++ nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
@@ -4,13 +4,14 @@
 // @downloadURL  https://raw.githubusercontent.com/edsantiago/greasemonkey/master/nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
 // @description  highlight diffs in nmlegis.gov bills
 // @include      https://nmlegis.gov/*/bills/*.html
-// @version      1.0
+// @version      1.1
 // @grant        none
 // ==/UserScript==
 
 /*
 ** Changelog
 **
+**  2023-01-14  1.1  Linkify bill in title
 **  2023-01-14  1.0  Initial version
 */
 var allSpans, thisSpan;
@@ -43,4 +44,17 @@ for (var i = 0; i < allSpans.snapshotLength; i++) {
     thisSpan = allSpans.snapshotItem(i);
     thisSpan.style["color"] = "#930";
     thisSpan.style["font-weight"] = "bold";
+}
+
+// page title: linkify bill
+allSpans = document.evaluate(
+    '//html/body/div[1]/p[1]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null);
+for (var i = 0; i < allSpans.snapshotLength; i++) {
+    thisSpan = allSpans.snapshotItem(i);
+		thisSpan.innerHTML = thisSpan.innerHTML.replace(/ ([HS])(B|CR|M|JR)0*([0-9]+)/,
+                                                    " <a href=\"https://nmlegis.gov/Legislation/Legislation?Chamber=$1&LegType=$2&LegNo=$3&year=23\"><b>$1$2$3</b></a>");
 }
