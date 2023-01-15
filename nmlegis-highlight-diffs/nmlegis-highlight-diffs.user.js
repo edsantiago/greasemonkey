b3cbe29cd8946a74bf8df21ae1c53ac5c9566d18
Eduardo Santiago <ed@edsantiago.com>
2023-01-15 12:59:27 -0700

    nmlegis: better linkifying of NMSA
    Signed-off-by: Eduardo Santiago <ed@edsantiago.com>

--- nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
+++ nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
@@ -4,13 +4,14 @@
 // @downloadURL  https://raw.githubusercontent.com/edsantiago/greasemonkey/master/nmlegis-highlight-diffs/nmlegis-highlight-diffs.user.js
 // @description  highlight diffs in nmlegis.gov bills
 // @include      https://nmlegis.gov/*/bills/*.html
-// @version      1.2
+// @version      1.3
 // @grant        none
 // ==/UserScript==
 
 /*
 ** Changelog
 **
+**  2023-01-15  1.3  Handle subsections (a-b-c.d) and "Chapter X, Article Y"
 **  2023-01-14  1.2  Linkify sections of NM code
 **  2023-01-14  1.1  Linkify bill in title
 **  2023-01-14  1.0  Initial version
@@ -69,7 +70,22 @@ allSpans = document.evaluate(
     null);
 for (var i = 0; i < allSpans.snapshotLength; i++) {
     thisSpan = allSpans.snapshotItem(i);
-    console.log(thisSpan);
-    thisSpan.innerHTML = thisSpan.innerHTML.replace(/(\d+)-(\d+)-(\d+)/,
-                                                    " <a href=\"https://law.justia.com/codes/new-mexico/2021/chapter-$1/article-$2/section-$1-$2-$3\" target=\"_blank\">$1-$2-$3 NMSA</a>");
+
+    var s = thisSpan.innerHTML;
+
+    // C-A-S.x NMSA
+    s = s.replace(/ (\d+)-(\d+)-(\d+)\.(\d+) NMSA/,
+                  " <a href=\"https://law.justia.com/codes/new-mexico/2021/chapter-$1/article-$2/section-$1-$2-$3-$4\" target=\"_blank\">$1-$2-$3.$4 NMSA</a>");
+
+    // C-A-S NMSA
+    s = s.replace(/(\d+)-(\d+)-(\d+) NMSA/,
+                  " <a href=\"https://law.justia.com/codes/new-mexico/2021/chapter-$1/article-$2/section-$1-$2-$3\" target=\"_blank\">$1-$2-$3 NMSA</a>");
+
+    // Chapter C, Section S
+    s = s.replace(/Chapter (\d+\w*), Article (\d+) NMSA/,
+                  function(match, chapter, article) {
+                      return " <a href=\"https://law.justia.com/codes/new-mexico/2021/chapter-" + chapter.toLowerCase() + "/article-" + article + "\" target=\"_blank\">" + match + "</a>";
+                  });
+
+    thisSpan.innerHTML = s;
 }
